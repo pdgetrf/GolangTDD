b508d118fd6dea07e804b06f35b683e1f62b7cfc
Peng Du <rick.peng.du@gmail.com>
2020-04-26 00:25:58 -0700

    basic TDD workflow

---
+++ hello/hello.go
@@ -0,0 +1,32 @@
+package main
+
+import _ "fmt"
+
+const spanish = "Spanish"
+const french = "French"
+const englishHelloPrefix = "Hello, "
+const spanishHelloPrefix = "Hola, "
+const frenchHelloPrefix = "Bonjour, "
+
+func Hello(name, language string) string {
+	if name == "" {
+		name = "World"
+	}
+	prefix := getPrefix(language)
+	return prefix + name
+}
+
+func getPrefix(language string) (prefix string) {
+	switch language {
+	case spanish:
+		prefix = spanishHelloPrefix
+	case french:
+		prefix = frenchHelloPrefix
+	default:
+		prefix = englishHelloPrefix
+	}
+	return
+}
+
+func main() {
+}
