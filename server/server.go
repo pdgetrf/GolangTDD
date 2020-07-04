8244082d6ef448be61e764d963903a637c2c7cf8
Peng Du <rick.peng.du@gmail.com>
2020-07-04 16:04:29 -0700

    add server example

---
+++ server/server.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+	"strings"
+)
+
+type PlayerStore interface {
+	GetPlayerScore(name string) int
+}
+
+type PlayerServer struct {
+	store PlayerStore
+}
+
+// implement this handlerfunc interface so it can be used in ListenAndServe
+func (p *PlayerServer) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	player := strings.TrimPrefix(r.URL.Path, "/players/")
+	fmt.Fprint(w, p.store.GetPlayerScore(player))
+}
